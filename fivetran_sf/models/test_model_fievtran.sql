edb16eccf97efbf2309a2ac93ab60baca24bdb8b
luisfigler <luis-figler@outlook.com>
2025-02-08 18:28:28 -0700

    fixing new model

--- fivetran_sf/models/test_model_fievtran.sql
+++ fivetran_sf/models/test_model_fievtran.sql
@@ -1,6 +1,22 @@
 WITH transformed1 AS (
   SELECT
-    tax_5_ rating quantity TIME invoice_id city gender product_line DATE payment branch cogs total gross_income customer_type unit_price gross_margin_percentage
+    tax_5_,
+    rating,
+    quantity,
+    TIME,
+    invoice_id,
+    city,
+    gender,
+    product_line,
+    DATE,
+    payment,
+    branch,
+    cogs,
+    total,
+    gross_income,
+    customer_type,
+    unit_price,
+    gross_margin_percentage
   FROM
     final_url_performance
 )
